8a700a1668143ad0b743b5746cd672a839a1d3b6
justYu2001 <jack4781012@gmail.com>
2021-07-01 20:00:58 +0800

    practice import

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
 import "../scss/index.scss";
 import "../index.html";
 
+import obj from "./obj";
+import add from "./add";
+
 console.log("hello world!");
 
 let arr = [1, 2, 3];
 
 arr.map((value) => console.log(value));
+
+console.log(obj.name);
+
+add(3, 4);
 
 if (module.hot) {
     module.hot.accept();
